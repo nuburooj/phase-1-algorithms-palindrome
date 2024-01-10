c3134d36083cdfe4d8bd792351182e075bc6cc36
Nuburooj Khattak <nuburoojkhattak@gmail.com>
2024-01-09 19:45:11 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+    set name of function
+    initialize function isPalindrome
+    takes in a word parameter
+    returns a word that is split into separate strings/characters
+    then is reversed and then joined back together
 */
 
 /*
   Add written explanation of your solution here
+   takes a word like mom or racecar,
+  splits it into individual characters,
+  then reverses them,
+  then joins them back together
 */
 
 // You can run `node index.js` to view these console logs
